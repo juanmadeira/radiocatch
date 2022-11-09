6fab66ff946deff821e37a7f76140ec595ff7eb4
Juan <juanpmad@outlook.com>
2022-11-09 15:24:45 -0300

    sw

---
+++ sw.js
@@ -0,0 +1,32 @@
+//consts utilizadas
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './',
+  './manifest.json'
+];
+
+//ao instalar
+self.addEventListener('install', event =>
+  event.waitUntil(cacheResources())
+)
+
+async function cacheResources() {
+  const cache = await caches.open(CACHE_NAME)
+  return cache.addAll(urlsToCache)
+}
+
+//ao fazer uma requisição (fetch
+
+self.addEventListener('fetch', event =>
+  event.respondWith(cachedResource(event.request))
+)
+
+async function cachedResource (req) {
+  const cache = await caches.open(CACHE_NAME)
+  const response = await cache.match(req)
+  if (response) {
+    return response;
+  }
+  return fetch(req);
+}
